b8fb79af98781314a35a21bed092d44e7f8c9f79
nagi125 <nagi125.dev@gmail.com>
2021-10-22 22:29:05 +0900

    子コンポーネントに処理を移動

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
+})
+export class TaskListComponent implements OnInit {
+
+  constructor() { }
+
+  title = "test-app";
+
+  tasks = [
+    {title: '牛乳を買う', done: false, deadline: new Date('2021-01-01')},
+    {title: '可燃ゴミを出す', done: true, deadline: new Date('2020-01-02')},
+    {title: '銀行に行く', done: false, deadline: new Date('2020-01-03')},
+  ];
+
+  newTask = {
+    title: '',
+    deadline: new Date(),
+  };
+
+  ngOnInit(): void {
+  }
+
+  addTask(): void {
+    this.tasks.push({title: this.newTask.title, done: false, deadline: new Date(this.newTask.deadline)});
+    this.newTask = {
+      title: '',
+      deadline: new Date(),
+    };
+  }
+
+  isOverdue(task: any) {
+    return !task.done && task.deadline < (new Date()).setHours(0, 0, 0, 0);
+  }
+
+}
