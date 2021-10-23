96f99ec51f5febc406e58b0f139c4a50fcb38236
nagi125 <nagi125.dev@gmail.com>
2021-10-23 21:24:24 +0900

    ループさせる部分を更にコンポーネント化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListItemComponent } from './task-list-item/task-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskListItemComponent
   ],
   imports: [
     BrowserModule,
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -32,9 +32,4 @@ export class TaskListComponent implements OnInit {
       deadline: new Date(),
     };
   }
-
-  isOverdue(task: any) {
-    return !task.done && task.deadline < (new Date()).setHours(0, 0, 0, 0);
-  }
-
 }
